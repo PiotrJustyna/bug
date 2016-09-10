5975ba5974fce1b522803485e43724a6c9c9c393
Piotr Justyna <piotr.justyna@gmail.com>
2016-09-10 11:38:12 +0100

    Minor experiments.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,7 @@
 module Main where
 
+import Network.Simple.TCP
+
 main :: IO ()
-main = putStrLn "bug"
+main = connect "127.0.0.1" "4242" $ \(connectionSocket, remoteAddr) -> do
+  putStrLn $ "Connection established to " ++ show remoteAddr
